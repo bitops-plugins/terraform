9763a3f3467a0a7087e6f5afd0dfebca847db8f5
Shyam <shyam.rayaprolu@gmail.com>
2022-02-22 19:55:40 +0530

    fixed tf install script

--- install.sh
+++ install.sh
@@ -9,35 +9,24 @@ echo ""
 
 apk info
 
-export TERRAFORM_VERSIONS=$(cat build.config.yaml | shyaml get-values terraform.versions)
-export HELM_VERSION=$(cat build.config.yaml | shyaml get-value helm.version)
-export KUBECTL_VERSION=$(cat build.config.yaml | shyaml get-value kubectl.version)
-export CLOUD_PLATFORM=$(cat build.config.yaml | shyaml get-value cloud_platform.name)
-export CI_PLATFORM=$(cat build.config.yaml | shyaml get-value ci_platform.name)
-export AWS_REGION=$(cat build.config.yaml | shyaml get-value cloud_platform.region)
-export CURRENT_ENVIRONMENT=$(cat build.config.yaml | shyaml get-value environment.default)
+# export TERRAFORM_VERSIONS=$(cat build.config.yaml | shyaml get-values terraform.versions)
+TERRAFORM_VERSION=$1
 
 
 mkdir -p /opt/download
 cd /opt/download
 
 function install_terraform() {
-    while IFS='' read -r version; do
-        TERRAFORM_DOWNLOAD_URL="https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_amd64.zip"
+    while IFS='' read -r TERRAFORM_VERSION; do
+        TERRAFORM_DOWNLOAD_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
         echo ${TERRAFORM_DOWNLOAD_URL}
-        curl -LO ${TERRAFORM_DOWNLOAD_URL} && unzip terraform_${version}_linux_amd64.zip -d ./
-        mv terraform /usr/local/bin/terraform-${version}
-        ln -s /usr/local/bin/terraform-${version} /usr/local/bin/terraform
-        chmod +x /usr/local/bin/terraform-${version}
-    done <<< "$TERRAFORM_VERSIONS"
+        curl -LO ${TERRAFORM_DOWNLOAD_URL} && unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d ./
+        mv terraform /usr/local/bin/terraform-${TERRAFORM_VERSION}
+        ln -s /usr/local/bin/terraform-${TERRAFORM_VERSION} /usr/local/bin/terraform
+        chmod +x /usr/local/bin/terraform-${TERRAFORM_VERSION}
+    done <<< "$TERRAFORM_VERSION"
 }
 
-function install_aws_iam_authenticator() {
-    curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.13.7/2019-06-11/bin/linux/amd64/aws-iam-authenticator
-    mv aws-iam-authenticator /usr/local/bin/
-    chmod u+x /usr/local/bin/helm /usr/local/bin/aws-iam-authenticator
-
-}
 
 install_terraform
-install_aws_iam_authenticator
+
